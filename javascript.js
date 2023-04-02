b494e6015c1a221e2ae3b437e2daab4fd354f0d2
Sushmit <sushmitdarje@iitbhilai.ac.in>
2023-04-03 04:09:45 +0530

    fourth commit

--- javascript.js
+++ javascript.js
@@ -2,3 +2,7 @@ const square=document.querySelectorAll('.square');
 const mole=document.querySelectorAll('.mole');
 const timeLeft=document.querySelector('#time-left');
 const message=document.querySelector('#action-message');
+let score = document.querySelector('#score');
+
+let result=0;
+let currentTime=timeLeft.textContent;
