7dd647e0ad3b9e3b2abdb6fe96e43af754727df3
Sushmit <sushmitdarje@iitbhilai.ac.in>
2023-04-03 04:21:19 +0530

    fifth commit

--- javascript.js
+++ javascript.js
@@ -6,3 +6,12 @@ let score = document.querySelector('#score');
 
 let result=0;
 let currentTime=timeLeft.textContent;
+function randomSquare(){
+    square.forEach(className=>{
+        className.classList.remove('mole');
+    })
+    let randomPosition=square[Math.floor(Math.random()*9)]
+    randomPosition.classList.add('mole')
+
+    hitPosition=randomPosition.id 
+}
